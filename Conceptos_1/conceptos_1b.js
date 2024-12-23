dad73347dd2d15b5ab0f30b7315b14a1a2ebee09
raulAM7 <raulartilesm@gmail.com>
2024-12-23 11:10:51 +0000

    update

--- Conceptos_1/conceptos_1b.js
+++ Conceptos_1/conceptos_1b.js
@@ -1,4 +1,17 @@
-const {saludar, sumarXY} = require("./conceptos_1")
+// ----------------------- 1.- EJEMPLOS DE MÓDULOS
 
+// Puedo importar mediante destructuring
+const {saludar, sumarXY} = require("./conceptos_1")
+console.log('Usando destructuring')
 console.log(saludar("raul"))
 console.log(sumarXY(1, 3))
+
+
+// O puedo importar todo el objeto y que las funciones sean métodos
+const funciones = require("./conceptos_1")
+console.log('Usando importación de un objeto completo')
+console.log(funciones.saludar("raul"))
+console.log(funciones.sumarXY(1, 3))
+
+
+// ----------------------- 1.- EJEMPLOS DE MÓDULOS
