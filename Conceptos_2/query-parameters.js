fc80f6fa8a62083efffe0e80f0ea26c12df350d6
raulAM7 <raulartilesm@gmail.com>
2024-12-23 21:34:13 +0000

    update

--- Conceptos_2/query-parameters.js
+++ Conceptos_2/query-parameters.js
@@ -12,7 +12,24 @@ router.get("/query-example", (req, res) => {
 // /route?param1key=param1value&param2key=param2value 
 
 
+// Ejemplos de rutas con query params
 
+// /products = {productId, catergory}
+
+router.get("/product", (req, res) => {
+    const {productId, category} = req.query
+    res.json({ProductID: productId, Category: category})
+})
+
+router.get("/search", (req, res) => {
+    const {busqueda} = req.query
+    res.json({Búsqueda: busqueda})
+})
+
+router.get("/page", (req, res) => {
+    const {number, size} = req.query
+    res.json({ Number: number, Size: size})
+})
 
 
 
