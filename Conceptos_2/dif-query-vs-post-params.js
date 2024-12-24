31a97a4e8029d184bf1b5c656dd32f3c7faac0df
raulAM7 <raulartilesm@gmail.com>
2024-12-24 19:54:51 +0000

    update

--- Conceptos_2/dif-query-vs-post-params.js
+++ Conceptos_2/dif-query-vs-post-params.js
@@ -3,9 +3,24 @@ let express = require('express')
 let router = express.Router()
 
 
+/*  ### **Ejercicio 1: Manejar Route Parameters**
 
+Crea una ruta que reciba un ID de usuario y un ID de pedido en el path:
 
+- Ruta: `/user/:userId/order/:orderId`
+- Respuesta:
+{ "userId": "123", "orderId": "456" }
 
+*/
+
+const userController = 
+{
+    getOneUserOrder: (req, res) => {
+        const {userId, orderId} = req.params
+        res.json({UserID: userId, OrderID: orderId})
+    }
+}
+router.get("/user/:userId/order/:orderId")
 
 
 module.exports = router
